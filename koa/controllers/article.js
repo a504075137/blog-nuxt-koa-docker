473216eaccab07919626dcb6a68ae1e42f4fb18c
luotianchun <luotianchun123@qq.com>
2020-12-06 16:30:33 +0800

    修改server

--- koa/controllers/article.js
+++ koa/controllers/article.js
@@ -1,23 +1,47 @@
 const list = [
     {
-        title: "JS基础",
-        key: 'luanxie',
-        children: [
-            {
-                id: 1,
-                type: 'link',
-                name: '防抖节流',
-                content: "https://www.yuque.com/u2025518/lgkgod/xkl0v6"
-            },
-            {
-                id: 2,
-                type: 'link',
-                name: '内存管理',
-                content: "https://www.yuque.com/u2025518/lgkgod/ny150b"
-            },
-        ]
+        authorId: "2jahxa",
+        authInfo: {
+            avatar: "http://203.195.144.87:3100/assets/images/avatar.jpg",
+            id: "2jahxa",
+            userName: "罗小淳"
+        },
+        category: "asaa",
+        categoryInfo: {
+            name: "JS基础",
+            id: "asaa"
+        },
+        title: "防抖节流",
+        createAt: "2020-11-20",
+        like: 10,
+        viewed: 100,
+        type: "yuque",
+        link: "https://www.yuque.com/u2025518/lgkgod/xkl0v6",
+        poster: "http://mingme.oss-cn-beijing.aliyuncs.com/image/poster/20201102/1604327850157_406ac87ec128cc97ada8a5d08220fa94.jpeg"
     },
+    {
+        authorId: "2jahxa",
+        authInfo: {
+            avatar: "http://203.195.144.87:3100/assets/images/avatar.jpg",
+            id: "2jahxa",
+            userName: "罗小淳"
+        },
+        category: "asaa",
+        categoryInfo: {
+            name: "JS基础",
+            id: "asaa"
+        },
+        title: "内存管理",
+        createAt: "2020-11-20",
+        like: 10,
+        viewed: 10,
+        type: "yuque",
+        link: "https://www.yuque.com/u2025518/lgkgod/ny150b",
+        poster: "http://mingme.oss-cn-beijing.aliyuncs.com/image/poster/20201102/1604327850157_406ac87ec128cc97ada8a5d08220fa94.jpeg"
+    },
+
 ]
+
 class Article {
     constructor(props) {
         this.params = props
